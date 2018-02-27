09c88d6ff7e50f8c1067465f6e2d5feb3b8cd1c0
Ian O'Connor <isoconnor89@gmail.com>
2018-02-27 23:43:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = { Diplo: "Lean On"};
+var playlist = new Object({ Diplo: 'Lean On' })
 
 function updatePlayList(playlist, artistName, songTitle){
   Object.assign({}, playlist, { [artistName]: songTitle });
