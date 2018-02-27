d783b3129d3d1b01b9bb801b037cbdcc22fece15
Ian O'Connor <isoconnor89@gmail.com>
2018-02-27 23:51:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
-var playlist = new Object({artistName: 'songTitle' })
+var playlist = new Object({artistName: 'songTitle' });
 
-function updateplaylist(playlist, artistName, songTitle){
+function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle;
   return playlist;
 }
