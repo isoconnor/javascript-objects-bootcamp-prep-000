98ad0e31a28aec03ca8c1c7227162671f5f9f8cd
Ian O'Connor <isoconnor89@gmail.com>
2018-02-27 23:53:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,4 +5,7 @@ function updatePlaylist(playlist, artistName, songTitle){
   return playlist;
 }
 
-
+function deleteFromPlaylist(playlist, artistName, songTitle){
+  delete playlist[artistName]
+  return playlist
+}
